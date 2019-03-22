1d20ab6d18910a9b1b28d2caa0d2e10f612315d1
poojabh37 <poojabh37@gmail.com>
2019-03-22 08:06:00 +0530

    adding another method

--- src/difficulty/school/strings/ReverseWords.java
+++ src/difficulty/school/strings/ReverseWords.java
@@ -7,18 +7,48 @@ public class ReverseWords {
 
     public static void main(String[] args) {
         String source = "pooja.bhatia.is.happy";
-        reverse(source);
+        reverseWords(source);
     }
 
     private static void reverseWords(String source) {
-        // String[] words = source.split("\\.");
-        List<String> words = splitString(source);
-        for (int i = words.size() - 1; i >= 0; i--) {
-            System.out.print(words.get(i) + ".");
+        String[] words = source.split("\\.");
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i]);
+            if (i > 0)
+                System.out.print(".");
+        }
+        System.out.println();
+    }
+
+    //method 3 --without Auxillary array
+    private static void reverse(String s) {
+        char[] revA = s.toCharArray();
+        reverseString(revA, 0, s.length() - 1);
+        int wordStart = 0;
+        int wordEnd;
+        for (int i = 0; i < s.length(); i++) {
+            if (revA[i] == '.') {
+                wordEnd = i - 1;
+                reverseString(revA, wordStart, wordEnd);
+                wordStart = i + 1;
+            } else if (i == s.length() - 1) {
+                reverseString(revA, wordStart, s.length() - 1);
+            }
+        }
+        System.out.println(new String(revA));
+    }
+
+    private static void reverseString(char[] revA, int wordStart, int wordEnd) {
+        for (int k = wordStart; k < wordEnd; k++, wordEnd--) {
+            char temp;
+            temp = revA[k];
+            revA[k] = revA[wordEnd];
+            revA[wordEnd] = temp;
         }
     }
 
-    //method 2  -- using auxillary array
+    //do not use this
+    //method 3  -- using auxillary array
     private static List<String> splitString(String source) {
         List<String> words = new ArrayList<>();
         char[] letters = source.toCharArray();
@@ -37,33 +67,5 @@ public class ReverseWords {
             }
         }
         return words;
-    }
-
-    //method 3 --without Auxillary array
-    private static void reverse(String s) {
-        char[] revA = s.toCharArray();
-        reverseString(revA, 0, s.length() - 1);
-        int wordStart = 0;
-        int wordEnd;
-        for (int i = 0; i < s.length(); i++) {
-            if (revA[i] == '.') {
-                wordEnd = i - 1;
-                reverseString(revA, wordStart, wordEnd);
-                wordStart = i + 1;
-            } else if (i == s.length() - 1) {
-                reverseString(revA, wordStart, s.length() - 1);
-            }
-
-        }
-        System.out.println(new String(revA));
-    }
-
-    private static void reverseString(char[] revA, int wordStart, int wordEnd) {
-        for (int k = wordStart; k < wordEnd; k++, wordEnd--) {
-            char temp;
-            temp = revA[k];
-            revA[k] = revA[wordEnd];
-            revA[wordEnd] = temp;
-        }
     }
 }
