1fceaf5376dccff757be1f209a36c2fe208596a9
bhatiap <bhatiap@optymyze.com>
2019-03-22 14:56:21 +0530

    adding questions, reafctoring

--- src/difficulty/school/strings/Pallindrome.java
+++ src/difficulty/school/strings/Pallindrome.java
@@ -1,7 +1,8 @@
 package difficulty.school.strings;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
+
+import static jdk.nashorn.internal.objects.NativeArray.reverse;
 
 public class Pallindrome {
 
@@ -17,9 +18,9 @@ public class Pallindrome {
     }
 
     private static void isPalindrome(String source) {
-        source = source.toLowerCase();                       //check regardless of case and special char including spaces
-        Pattern regex = Pattern.compile("[^A-Za-z0-9]");
-        source = source.replaceAll(regex.pattern(), "");
+//        source = source.toLowerCase();                       //check regardless of case and special char including spaces
+//        Pattern regex = Pattern.compile("[^A-Za-z0-9]");
+//        source = source.replaceAll(regex.pattern(), "");
 
         if (source.equals(reverse(source))) {
             System.out.println("YES");
@@ -27,19 +28,5 @@ public class Pallindrome {
         }
         System.out.println("No");
     }
-
-    private static String reverseString(String source) {
-        return new StringBuilder(source).reverse().toString();
-    }
-
-    private static String reverse(String source) {
-        char[] origA = source.toCharArray();
-        char[] reverseA = new char[source.length()];
-        for (int i = source.length() - 1; i >= 0; i--) {
-            reverseA[source.length() - 1 - i] = origA[i];
-        }
-        return new String(reverseA);
-    }
-
 
 }
