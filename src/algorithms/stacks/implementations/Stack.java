c212562f0ac104edb9a1aa2c140ca351bf842f24
poojabh37 <poojabh37@gmail.com>
2019-07-31 18:06:18 +0530

    adding comments
    Signed-off-by: poojabh37 <poojabh37@gmail.com>

--- src/algorithms/stacks/implementations/Stack.java
+++ src/algorithms/stacks/implementations/Stack.java
@@ -1,11 +1,15 @@
 package algorithms.stacks.implementations;
 
+//follows LIFO (Last In First Out)
 public interface Stack {
 
+    //add new element on to the top of the stack
     void push(int item);
 
+    //deletes and returns the top element
     int pop();
 
+    //returns the top element
     int peek();
 
     boolean isEmpty();
