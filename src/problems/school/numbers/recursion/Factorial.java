2f9639d7cc71144c0567ee020d18e2042a48db70
poojabh37 <poojabh37@gmail.com>
2019-08-12 23:40:58 +0530

    refactoring
    Signed-off-by: poojabh37 <poojabh37@gmail.com>

--- src/problems/school/numbers/recursion/Factorial.java
+++ src/problems/school/numbers/recursion/Factorial.java
@@ -6,22 +6,15 @@ public class Factorial {
 
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
-            int t = input.nextInt();
             int num = input.nextInt();
-            for (int i = 0; i < t; i++) {
-                long temp = 1;
-                long fact = factorial(num, temp);
-                System.out.println(fact);
-            }
+            int fact = factorial(num);
+            System.out.println(fact);
         }
     }
 
-    private static long factorial(int num, long temp) {
-        if (num > 0) {
-            temp = temp * num;
-            return factorial(num - 1, temp);
-        }
-        return temp;
+    private static int factorial(int num) {
+        if (num == 1) return 1;
+        return num * factorial(num - 1);
     }
 
 }
